bc835e670d63195a0afb59da980fa4fa7f04f5fe
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2017-08-18 06:22:04 +0200

    Add InsertHandler
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 
@@ -28,5 +31,43 @@ func main() {
 	)
 	if err != nil {
 		panic(err)
+	}
+
+	// HTTP server stuff.
+	http.HandleFunc("/insert", InsertHandler(db))
+	http.ListenAndServe(":8080", nil)
+}
+
+// InsertHandler returns the insert handler.
+func InsertHandler(db *tsdb.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		buf, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		defer r.Body.Close()
+
+		metrics, err := LineToMetrics(buf)
+		if err != nil {
+			panic(err)
+		}
+
+		// Add logic.
+		app := db.Appender()
+		for _, metric := range metrics {
+			ref, err := app.Add(metric.Series, metric.Timestamp, metric.Value)
+			if err != nil {
+				panic(err)
+			}
+			// ref can be used to do app.AddFast(ref, ts, val) but here we just don't use it.
+			_ = ref
+		}
+
+		if err := app.Commit(); err != nil {
+			panic(err)
+		}
+		// We can also do app.Rollback() which would just drop everything to the floor.
+
+		fmt.Fprintln(w, "Success")
 	}
 }
